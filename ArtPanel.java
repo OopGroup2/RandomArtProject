a6a314a301a7ea7b27c4e68803f219047463a96d
oopgroup2 <monkeyextream@hotmail.com>
2013-11-14 22:17:56 +0700

    update the method makeColor(), paintColor()

--- ArtPanel.java
+++ ArtPanel.java
@@ -17,12 +17,34 @@ class ArtPanel extends JPanel {
  
  //To paint the color
  public void paintColor(Graphics g) {
- }
+    for (int j = 0; j < getHeight(); j++) {
+        for (int i = 0; i < getWidth(); i++) {
+            double x = ((((double) i) / getWidth()) * 2.0) - 1.0;
+            double y = ((((double) j) / getHeight()) * 2.0) - 1.0;
+    
+            double red   = rgbExpr[0].evaluate(x, y);
+            double green = rgbExpr[1].evaluate(x, y);
+            double blue  = rgbExpr[2].evaluate(x, y);
+    
+            Color col = new Color(makeColor(red), makeColor(green),makeColor(blue));
+            g.setColor(col);
+            g.fillRect(i, j, 1, 1);
+        }
+    }
+}
  
  //To make the color in the rank between to 0-255
  public int makeColor(double num){
-  
-  return 0;
- }
+      num += 1.0;
+      if (num < 0) {
+          num *= -1;
+        }
+        num /= 2.0;
+        int color = (int) (num * 255);
+        if (color >= 255) {
+            color = 255;
+        }
+        return color;
+    }
 }
 
