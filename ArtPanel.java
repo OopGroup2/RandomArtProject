9ce859dfdc3b706b70756ef5d8d615829352bd46
oopgroup2 <monkeyextream@hotmail.com>
2013-11-17 21:46:11 +0700

    update the tree function

--- ArtPanel.java
+++ ArtPanel.java
@@ -1,43 +1,44 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
+
+
 class ArtPanel extends JPanel {
- private CreateNode[] rgbExpr;
+ private CreateNode[] exprRGB;
+ 
+ 
  
  public ArtPanel(int width, int height) {
-  rgbExpr = new  CreateNode[3];
+  exprRGB = new  CreateNode[3];
+  setSize(new Dimension(width, height));
  }
- 
- //To set color,red,green,blue in the function.
- public void setRGBExpr(int rgb, CreateNode expr) {
-  
- }
- 
+
  //To paint the color
- public void paintColor(Graphics g) {
-    for (int j = 0; j < getHeight(); j++) {
+ public void paint(Graphics g) {
+   for (int j = 0; j < getHeight(); j++) {
         for (int i = 0; i < getWidth(); i++) {
             double x = ((((double) i) / getWidth()) * 2.0) - 1.0;
             double y = ((((double) j) / getHeight()) * 2.0) - 1.0;
     
-            double red   = rgbExpr[0].evaluate(x, y);
-            double green = rgbExpr[1].evaluate(x, y);
-            double blue  = rgbExpr[2].evaluate(x, y);
+            double red   = exprRGB[0].evaluate(x, y);
+            double green = exprRGB[1].evaluate(x, y);
+            double blue  = exprRGB[2].evaluate(x, y);
     
             Color col = new Color(makeColor(red), makeColor(green),makeColor(blue));
             g.setColor(col);
             g.fillRect(i, j, 1, 1);
         }
     }
-}
+ }
  
  //To make the color in the rank between to 0-255
  public int makeColor(double num){
-      num += 1.0;
+   num =num + 1.0;
       if (num < 0) {
-          num *= -1;
+          num = num * -1;
         }
         num /= 2.0;
         int color = (int) (num * 255);
@@ -46,5 +47,10 @@ class ArtPanel extends JPanel {
         }
         return color;
     }
-}
+    
+  //To set color,red,green,blue in the function./*
+ public void rgbString(int rgb, CreateNode expr) {
+   exprRGB[rgb] = expr;
+ }
+ }
 
--- CreateNode.java
+++ CreateNode.java
@@ -1,36 +1,67 @@
 import java.lang.Math;
 import java.util.Random;
- class CreateNode {
-     
-  private CreateNode leaf;
+class CreateNode {
+  private CreateNode leafNode;
   private String textFunction;
   private int randomNumber;
+  private static final int RANDOM_SINCOS = 2;
+  private static final int RANDOM_XY     = 2;
+ 
   
   public CreateNode(CreateNode leaf) {
-   // addLeaf(leaf);
+    addLeaf(leaf);
   }
-  
   public CreateNode() {
-    this.leaf = null;
+    this.leafNode = null;
   }
   
   public void addLeaf(CreateNode leaf){
-    this.leaf = leaf;
+    this.leafNode = leaf;
+    random();
   }
   
+  public void random(){
+    if(leafNode != null){
+      this.randomNumber = (int)(Math.random()*RANDOM_SINCOS);
+    }else{
+      this.randomNumber = (int)(Math.random()*RANDOM_XY+RANDOM_SINCOS);
+    }  
+    if(this.randomNumber == 0){
+      this.textFunction = "sin(PI*x";
+    }else if(this.randomNumber ==1){
+      this.textFunction = "cos(PI*y";
+    }else if(this.randomNumber ==2){
+      this.textFunction = "x";
+    }else{
+      this.textFunction = "y";
+    }
+  }
+      
+ 
   
   /* /**
   * Evaluate the function represented by this node
   * for the given x and y values.
   */
   public double evaluate(double x, double y){
-    return 0;
+    if(this.randomNumber == 0){
+      return Math.sin(Math.PI *x* this.leafNode.evaluate(x, y));
+    }else if(this.randomNumber ==1){
+      return Math.cos(Math.PI *y* this.leafNode.evaluate(x, y));
+    }else if(this.randomNumber ==2){
+      return x;
+    }else{
+      return y;
+    }  
   }
  
  //This method will show the function of random art in the string
-  public String funcToString(){
-    return "";
- 
-  }
+ public String stringFunction(){
+    if(this.leafNode == null){
+      return "("+ this.textFunction +")";
+    }else{
+       return  this.textFunction + " "+ this.leafNode.stringFunction();
+    }
+ }
 }
 
